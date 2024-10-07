679977dfdfc9766c235afccecfa9dd42354850d4
admin <admin@DESKTOP-IR84QRD>
2024-10-08 00:59:09 +1000

    2

---
+++ Source/cppBase/Private/CppHubActor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CppHubActor.h"
+
+// Sets default values
+ACppHubActor::ACppHubActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACppHubActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACppHubActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
